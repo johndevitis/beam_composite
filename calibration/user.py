c39a8a95d5ff20df97abe6bed8e1a20cde62aa09
John DeVitis <johndevitis@gmail.com>
2018-01-11 23:06:26 -0500

    removed old code from user.py

--- calibration/user.py
+++ calibration/user.py
@@ -6,13 +6,12 @@ from settings import *
 import st7py as st7
 
 
-
 # load meta data from excel file as a pandas dataframe
 meta = pd.read_excel(XLS_FILE, sheet_name=None)
 
 
 def set_barrier_modulus(uid,value):
-    print('Setting {}...'.format(value))
+    print('Setting...')
 
 
 def set_deck_modulus(uid,value):
@@ -69,63 +68,3 @@ setters = {'barrier_modulus': set_barrier_modulus,
            'kx_mid': set_kx_mid,
            'steel_modulus': set_steel_modulus,
            'diaphragm_modulus': set_diaphragm_modulus}
-
-
-
-def sensitivity():
-    f, df, paras = assignment_functions()
-    # loop included parameters
-    for idx, para in paras.iterrows():
-        # get array of alphas based on lower and upper bounds
-        alphas = np.linspace(para['x_lb'], para['x_ub'], PARALLEL_NUM_WORKERS)
-        # scale alpha values (scale_para broadcasts here, i.e. works for scalar and array of alphas)
-        values = scale_para(para, alphas)
-
-
-
-
-def run_nfa(uid):
-    # load St7API
-    start()
-
-    # open st7 model from st7py Model class instance
-    model = Model(filename = ST7_FILE, scratch = ST7_SCRATCH, uid = uid)
-    model.open()
-
-    # get total number of elements
-    tots = model.totals()
-
-    # run NFA solver using model filename and uid to name files
-    nfa_file, nfa_log = gen_result_name(model.filename, uid,'.NFA','.NFL')
-    nfa = NFA(uid = uid,
-              filename = nfa_file,
-              logname = nfa_log,
-              fcase = NFA_FCASE,
-              nsm = NFA_NSM,
-              nmodes = NFA_NMODES)
-
-    # run  nfa solver
-    nfa.run(disp=True)
-
-    freqs = nfa.getFrequencies()
-    shapes = nfa.getModeShapes(nodes=np.arange(1,1334))
-
-    # close model file and unload St7API
-    model.close()
-    stop()
-
-    return freqs, shapes
-
-
-
-
-def calibrate():
-    pass
-
-
-
-def main(uid):
-
-
-    # load xls file w/ meta data
-    paras = pd.read_excel(XLS_FILE, sheet_name='parameters')
